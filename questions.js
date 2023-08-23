50b1b79c27c1333ae383223c2de33e9f0ee79509
John Davis <johndavisambassa@gmail.com>
2023-08-23 19:25:19 +0100

    quiz-sans selecteur

---
+++ questions.js
@@ -0,0 +1,97 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Question 1?",
+        answer: "A. Lorem ipsum dolor sit.",
+        options: [
+            "A. Lorem ipsum dolor sit.",
+            "B. douala",
+            "C. bafoussam",
+            "D. bertoua"
+        ]
+    },
+    {
+        numb: 2,
+        question: "",
+        answer: "",
+        options: [
+            "A. yaounde",
+            "B. douala",
+            "C. bafoussam",
+            "D. bertoua"
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        options: [
+            "A. yaounde",
+            "B. douala",
+            "C. bafoussam",
+            "D. bertoua"
+        ]
+    },
+    {
+        numb: 4,
+        question: "czao vkjrk gerg pur eor ",
+        answer: "",
+        options: [
+            "A. yaounde",
+            "B. douala",
+            "C. bafoussam",
+            "D. bertoua"
+        ]
+    },
+    {
+        numb: 5,
+        question: "godeoo c ec wlncz c mze",
+        answer: "",
+        options: [
+            "A. yaounde",
+            "B. douala",
+            "C. bafoussam",
+            "D. bertoua"
+        ]
+    },
+    {
+        numb: 6,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 7,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 8,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 9,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 10,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+];
---
+++ script.js
@@ -0,0 +1,95 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
+    questionCounter(1);
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if (questionCount < questions.length - 1){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+    }
+    else {
+        console.log('Question complétée')
+    }
+    
+}
+
+
+
+
+const optionList = document.querySelector('.option-list');
+
+// revue des questions et options des listes
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text')
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+optionList.innerHTML = optionTag;
+
+const option = document.querySelectorAll('.option');
+for (let i = 0; i < option.length; i++){
+    option[i].setAttribute('onclick', 'optionSelected(this)');
+}
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+    
+    
+
+    if (userAnswer == correctAnswer) {
+        console.log('reponse correcte');
+        answer.classList.add('correct');
+    }
+    else {
+        console.log('reponse incorrecte');
+        answer.classList.add('incorrect');
+    }
+
+// si l'utilisateur a selectionné une question, desactiver le reste
+
+
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} sur ${questions.length} Questions`;
+}
